4309061f511d6d02f08fffaf3175afde5b718ee0
Cody Johnston <90635630+ILHS1101328@users.noreply.github.com>
2022-04-05 20:08:52 +0000

    Design Completed v2

--- EMP.java
+++ EMP.java
@@ -1,45 +1,20 @@
 import java.util.*;
 class EMP {
   public static void main(String[] args) {
+// Scanner Console 
     Scanner Console = new Scanner(System.in);
     Random rand = new Random();
-    // Introduction method
-    introduction();
-    // Summary method
-    summary();
-  }
-  // introduction method defined
-  // explains the point of the code 
-  // prints once
-  public static void introduction() {
-    System.out.println("Welcome to the EMP! (Elementary Math Program)");
-    System.out.println("This program allows you to take a series of quizzes");
-    System.out.println("with a mix of addition, subtraction, multiplication,");
-    System.out.println("and division problems. It will keep track of your");
-    System.out.println("scores on each quiz, and give you a summary of your");
-    System.out.println("results at the end. Good skill!");
-    System.out.println();
-  }
-
-  // required correct gets info from constructer
-  // then returns the required
-  public requiredCorrectMethod(int numberQuiz, Scanner Console) {
-    System.out.print("Quiz #" + numberQuiz + ": Number of correct answers required? ");
-    int requiredCorrect = Console.nextInt();
-    if (requiredCorrect > 0) {
-      return requiredCorrect;
-    }
-  }
-  // method that gets the question
-  public getQuestion(int numberQuiz, int number1, int number2, Scanner Console) {
-    while (numberQuiz ) {
-      
-    }
-  }
-  // after questions are answered print the results that include the % and the attempts compared to questions
-  public quizresults() {
+// Introduction method
     
+// Method to administer a single quiz (just one quiz, not multiple quizzes)
+    
+// OverallResults method
   }
+// Introduction method defined with blank println following
 
+// Method that administers the quiz
+// -Required inside of quiz method is while loop
+// -Inside this method will include println's and input
 
+// Method that prints Overall results
 }
